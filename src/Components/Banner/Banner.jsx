262e2b731d5058762e614e3d301d4673046371f1
sam-z-mridula <sameehamridula@gmail.com>
2025-10-08 18:03:39 +0600

    navbar and header added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import GooglePlay from "../../assets/googlePlay.png";
+import AppStore from "../../assets/AppStore.png";
+import heroImg from "../../assets/hero.png"
+import { Link } from "react-router";
+
+const Banner = () => {
+  return (
+    <div className="my-20 text-center">
+      <div>
+        <h1 className="font-bold text-6xl leading-tight text-[#192f45]">
+          We Build <br /> <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-transparent bg-clip-text">Productive</span> Apps
+        </h1>
+        <p className="text-gray-500 mt-5 mb-8">
+          At HERO.IO, we craft innovative apps designed to make everyday life
+          simpler, smarter, and more exciting. <br /> Our goal is to turn your
+          ideas into digital experiences that truly make an impact.
+        </p>
+
+        <div className="flex gap-5 w-fit mx-auto">
+
+          <Link to={'https://play.google.com/store/games?hl=en'} className="btn py-7 flex items-center gap-3">
+            <img src={GooglePlay} alt="" />
+            <span className="text-lg font-semibold">Google Play</span>
+          </Link>
+
+          <Link to={'https://www.apple.com/app-store/'} className="btn py-7 flex items-center gap-3">
+            <img src={AppStore} alt="" />
+            <span className="text-lg font-semibold">App Store</span>
+          </Link>
+
+        </div>
+
+      </div>
+      <div className="w-[60%] mx-auto mt-8">
+        <img className="mx-auto" src={heroImg} alt="" />
+      </div>
+
+      <div className="p-20 bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white">
+        <h1 className="text-4xl font-semibold">Trusted By Million, Built For You</h1>
+        <div className="mt-8 flex gap-30 w-fit mx-auto">
+            <div>
+                <p className="font-light text-gray-300 text-sm">Total Downloads</p>
+                <h1 className="text-6xl font-bold my-3">29.6M</h1>
+                <p className="font-light text-gray-300 text-sm">21% more than last month</p>
+            </div>
+            <div>
+                <p className="font-light text-gray-300 text-sm">Total Reviews</p>
+                <h1 className="text-6xl font-bold my-3">906K</h1>
+                <p className="font-light text-gray-300 text-sm">46% more than last month</p>
+            </div>
+            <div>
+                <p className="font-light text-gray-300 text-sm">Active Apps</p>
+                <h1 className="text-6xl font-bold my-3">132+</h1>
+                <p className="font-light text-gray-300 text-sm">31 more will Launch</p>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
+import Root from '../Pages/Root/Root';
+import ErrorPage from '../Pages/Error/ErrorPage';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+    ]
   },
 ]);
 
